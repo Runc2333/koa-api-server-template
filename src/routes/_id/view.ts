710b1a47f53e6845ad445f41ee4ceca4f0638745
Runc2333 <i@runc321.com>
2023-05-28 02:30:46 +0800

    remove console.log statements

--- src/routes/_id/view.ts
+++ src/routes/_id/view.ts
@@ -5,7 +5,6 @@ import stdrtn from "@/utils/stdrtn";
 export default [{
   method: "get",
   handler: async (ctx: Koa.Context) => {
-    console.log(ctx.params.id);
     stdrtn.success(ctx.res, `Wanna get ${ctx.params.id}?`);
   },
 }];
